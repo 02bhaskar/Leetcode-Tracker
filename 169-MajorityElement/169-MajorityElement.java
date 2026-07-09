fc51e60e2232982bb4fd1de0db1ebdec9be20ce1
02bhaskar <bhaskar272006@gmail.com>
2026-07-09 15:07:59 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,8 @@
+// Last updated: 7/9/2026, 3:07:59 PM
+class Solution {
+    public int majorityElement(int[] nums) {
+      int n = nums.length;
+      Arrays.sort(nums);
+      return nums[n/2];
+    }
+}
