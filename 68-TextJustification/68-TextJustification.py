f96427d5f814cbfeef8bf112d35385c5b87bc426
02bhaskar <bhaskar272006@gmail.com>
2026-07-09 15:08:31 +0530

    Create 68-TextJustification

---
+++ 68-TextJustification/68-TextJustification.py
@@ -0,0 +1,13 @@
+# Last updated: 7/9/2026, 3:08:30 PM
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        res, line, width = [], [], 0
+
+        for w in words:
+            if width + len(w) + len(line) > maxWidth:
+                for i in range(maxWidth - width): line[i % (len(line) - 1 or 1)] += ' '
+                res, line, width = res + [''.join(line)], [], 0
+            line += [w]
+            width += len(w)
+
+        return res + [' '.join(line).ljust(maxWidth)]
