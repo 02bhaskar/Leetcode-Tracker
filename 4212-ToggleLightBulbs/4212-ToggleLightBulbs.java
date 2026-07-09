6105e53905b1d6fb781bdc54a23cd8d95a76c681
02bhaskar <bhaskar272006@gmail.com>
2026-07-09 15:05:23 +0530

    Create 4212-ToggleLightBulbs

---
+++ 4212-ToggleLightBulbs/4212-ToggleLightBulbs.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 3:05:22 PM
+class Solution {
+    public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
+        Set<Integer>a = new HashSet<>();
+            for (int i = 0;i<bulbs.size();i++){
+                int b = bulbs.get(i);
+                if(a.contains(b)){
+                    a.remove(b);  
+                }else{
+                    a.add(b);
+                }
+            }
+        List<Integer> c=new ArrayList<>(a);
+        Collections.sort(c);
+        return c;
+    }
+}
