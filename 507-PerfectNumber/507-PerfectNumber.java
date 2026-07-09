e4bedb3554c09a19c5d1f35c75e838eab56cfd75
02bhaskar <bhaskar272006@gmail.com>
2026-07-09 15:06:41 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 3:06:40 PM
+class Solution {
+    public boolean checkPerfectNumber(int num){
+        if (num<=1) return false;
+        int sum = 1;
+        for(int i=2;i*i <=num; i++){
+            if(num%i==0){
+                sum+=i;
+                int other = num / i;
+                if (other !=i) sum += other;
+            }
+        }
+        return sum==num;
+    }
+}
