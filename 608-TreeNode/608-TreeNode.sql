ea0c07a51cb2f3210272fc9ba46aca2e511e8717
02bhaskar <bhaskar272006@gmail.com>
2026-07-17 09:35:21 +0530

    Create 608-TreeNode

---
+++ 608-TreeNode/608-TreeNode.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/17/2026, 9:35:18 AM
+SELECT id,
+
+    CASE 
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM Tree)THEN 'Inner'
+        ELSE 'Leaf'
+        END AS type
+ FROM Tree
+		
